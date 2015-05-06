28e8bebf6303787bb45c818d20ffd4f109e183af
Nicolas Vila <nicolas@nicolas-netbook>
2015-05-06 12:48:15 +0200

    Update

--- scraper.py
+++ scraper.py
@@ -57,7 +57,7 @@ def parse_list(root):
             'telephone': telephone,
             'description': description,
         }
-        scraperwiki.sqlite.save(unique_keys=['source_url'], data=record)
+        scraperwiki.sqlite.save(unique_keys=['source_url'], data=record, table_name="tripadvisor")
         #scraperwiki.sqlite.save_var('lastindex', index)
 
 def scrape_activities_in_a_region(url):
