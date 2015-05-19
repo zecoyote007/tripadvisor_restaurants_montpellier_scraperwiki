5d058f00ba9be0712628537cb1a162fc72e9aa42
zecoyote007 <nicolas@nicolas-netbook>
2015-05-19 15:01:30 +0200

    Fixed pagination

--- scraper.py
+++ scraper.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from datetime import datetime
 import scraperwiki
 import lxml.html
 import urllib2
@@ -7,15 +10,23 @@ import HTMLParser
 from lxml import etree
 from pyquery import PyQuery as pq
 
-#Montpellier
-#scrape_url = "http://www.tripadvisor.fr/RestaurantSearch?geo=187153"
-#Villeneuve-Les-Maguelone
-#scrape_url = "http://www.tripadvisor.fr/RestaurantSearch?geo=1221643"
-#Castres
-scrape_url = "http://www.tripadvisor.fr/RestaurantSearch?geo=187169&o=a%s&sortOrder=popularity"
+#area = "187153"	#Montpellier
+#area = "1221643"	#Villeneuve-Les-Maguelone
+#area = "187169"	#Castres
+#area = "665880"	#Carnon
+
+#area = "187149"	#Languedoc-Roussillon (7613)
+
+area = "2462549"	#Aude (1107)
+#area = "2462552"	#Lozère (199/204)
+#area = "1774988"	#Pyrénées-Orientales (1429)
+#area = "2431928"	#Hérault (3028)
+#area = "2431943"	#Gard (1509)
+
+scrape_url = "http://www.tripadvisor.fr/RestaurantSearch?geo=" + area + "&o=a%s&sortOrder=popularity"
 
 header = { 'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.47 Safari/536.11',
-           'Cookie': 'TASession=%1%V2ID.25F62354D4F10DF7FFE74EAFC0FF81E3*SQ.2*LS.UserReviewController*GR.34*TCPAR.13*TBR.55*EXEX.39*ABTR.73*PPRP.83*PHTB.6*FS.76*CPU.50*HS.popularity*ES.popularity*AS.popularity*DS.5*SAS.popularity*FPS.oldFirst*DF.0*LP.%2FRestaurantSearch%3Fgeo%3D1221643*MS.19*RMS.-1*TRA.true*LD.1221643*FBH.2; SessionTest=true; CommercePopunder=SuppressUnload; ServerPool=X; TATravelInfo=V2*A.2*MG.-1*HP.2*FL.3*RS.-1; CM=%1%HanaPersist%2C%2C-1%7Cpu_vr2%2C%2C-1%7Ct4b-pc%2C%2C-1%7CHanaSession%2C%2C-1%7CFtrSess%2C%2C-1%7CRCPers%2C%2C-1%7CHomeAPers%2C%2C-1%7C+r_lf_1%2C%2C-1%7CWShadeSeen%2C%2C-1%7CRCSess%2C%2C-1%7C+r_lf_2%2C%2C-1%7Cpu_vr1%2C%2C-1%7CFtrPers%2C%2C-1%7CHomeASess%2C%2C-1%7CBPPers%2C1%2C1431507941%7CBPSess%2C1%2C-1%7CPU_quick1%2C%2C-1%7CPU_quick2%2C%2C-1%7Cvr_npu2%2C%2C-1%7CLastPopunderId%2C94-610-34217%2C-1%7Cpssamex%2C%2C-1%7Cvr_npu1%2C%2C-1%7Cbrandsess%2C%2C-1%7CCCPers%2C%2C-1%7CCCSess%2C%2C-1%7CWAR_RESTAURANT_FOOTER_SESSION%2C%2C-1%7Cbrandpers%2C%2C-1%7Csesssticker%2C%2C-1%7C%24%2C%2C-1%7Ct4b-sc%2C%2C-1%7Cviator_2%2C%2C-1%7CWarPopunder_Session%2C%2C-1%7Csess_rev%2C%2C-1%7Csessamex%2C%2C-1%7Cviator_1%2C%2C-1%7CWarPopunder_Persist%2C%2C-1%7CSaveFtrPers%2C%2C-1%7Cr_ta_2%2C%2C-1%7Cr_ta_1%2C%2C-1%7CSaveFtrSess%2C%2C-1%7Cpers_rev%2C%2C-1%7CRBASess%2C%2C-1%7Cperssticker%2C%2C-1%7CMetaFtrSess%2C%2C-1%7CRBAPers%2C%2C-1%7CWAR_RESTAURANT_FOOTER_PERSISTANT%2C%2C-1%7CMetaFtrPers%2C%2C-1%7C; TASSK=enc%3ARNniFej67m%2BOS2WmzABITVIvwBRyE34H5yY%2FWEDeRgajeI4bPVxeHAdUPz3chjcM; TAUnique=%1%enc%3AIhZfEIVqUMIbljTEYWK8objYfixl0BnB06b1atePIxQ2jHwltRJPGQ%3D%3D; roybatty=AAABTSh5M36KsTYAzgyHlHu%2BCh%2BGxCeEiP6F8w%3D%3D%2C1'}
+           'Cookie': ''}
 
 email_regex = re.compile(r'(\b[\w.]+@+[\w.]+.+[\w.]\b)')
 h = HTMLParser.HTMLParser()
@@ -50,6 +61,7 @@ def strip_tags(value):
     return ""
 
 def parse_page(root):
+    page_records = 0
     """ Takes a listing page and indexes all the listings in it """
     for el in root(".listing a.property_title"):
         page_url = "http://www.tripadvisor.fr" + el.get("href")
@@ -95,28 +107,63 @@ def parse_page(root):
         scraperwiki.sqlite.save(unique_keys=['source_url'], data=data, table_name="tripadvisor")
         #scraperwiki.sqlite.save_var('lastindex', index)
 
+	page_records = page_records + 1
+    return page_records
+
 def parse_listing_pages(start_url):
+    start_time = datetime.now()
+    print start_time
     # not iterate over the pages
-    count = 0
+    currentPage = 1
+    total_records = 0
     while True:
-	# targets each page in the list
-        url = start_url % (count)
+	if currentPage == 1:
+            url = start_url
         print "PROCESSING PAGE %s" % url
 	print "-" * 40
-        root = get_url(url)
-
-        # check if there are items, if not stop since you exceeded the total pages
-        if not root(".listing"):
-            print "END OF LISTING REACHED AT PAGE %s" % count
-            break
+        page = get_url(url)
 
         # this will parse the first listing page
-        parse_page(root)
-        print "FINISHED PAGE %s" % count
-        count = count + 30
-        
+        nb_records = parse_page(page)
+        total_records = total_records + nb_records
+
+	print "FINISHED PAGE %s" % currentPage
+
+	#Look for next page link
+	next_page_url = page('.current').next().attr('href')
+	if next_page_url:
+		url = 'http://www.tripadvisor.fr' + next_page_url
+		currentPage = currentPage + 1
+	else:
+		print "END OF LISTING REACHED AT PAGE %s" % currentPage
+		break
+    end_time = datetime.now()
+    delta_time = end_time - start_time
+    print "PROCESSING DURATION %s" % delta_time
+    print "RECORDS PROCESSED %s" % total_records
+    average_time_per_record = delta_time.seconds / total_records
+    print "SECONDS PER RECORD = %s" % average_time_per_record
+
+def parse_page_links(scrape_url):
+	page = get_url(scrape_url)
+	next_page_url = page('.current').next().attr('href')
+        if next_page_url:
+                print 'http://www.tripadvisor.fr' + next_page_url
+	else:
+                print "END OF LISTING REACHED"
+
+def test_ip_address():
+        page = get_url('http://checkip.dyndns.org')
+	print page("body").html()
+
+test_ip_address()
+
+# Parse only the first page (30 records)
 #scrape_url_page = get_url(scrape_url)
 #parse_page(scrape_url_page)
 
-parse_listing_pages(scrape_url)
+# Parse next pages URLs
+#parse_page_links(scrape_url)
 
+# Parse all the records
+parse_listing_pages(scrape_url)
